89643d4cd4801f7d941576c65affd7e1766633e2
ian29012 <iancheng29012@gmail.com>
2023-01-21 20:35:17 +0000

    updated the writrFile function and the correct path of the generateMarkdown.js

--- Start/README.md
+++ Start/README.md
@@ -1,39 +1,38 @@
 
-# 2
-
+# 1
+  
 ## Table of Contents 
-        
+          
 [Description](#Description)  &nbsp; | &nbsp;  [Installation](#Installation)  &nbsp; | &nbsp;  [Usage](#Usage)  &nbsp; | &nbsp;  [Contributing](#Contributing)  &nbsp; | &nbsp;  [Tests](#Tests)  
-        
-![alt](https://img.shields.io/github/license/2/2?style=for-the-badge)
-![alt](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)
-
+          
+![alt](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
+  
 ## Description
-        
-2
-    
+          
+1
+      
 ## Installation
-    
-2
-        
+      
+1
+          
 ## Usage
-    
-2
-        
+      
+1
+          
 ## Contributing
-        
-2
-        
+          
+1
+          
 ## Tests
-        
-2
-        
+          
+1
+          
 ## license
-
-The BSD 2-Clause “Simplified” License covers this application
-        
+  
+The Apache License covers this application
+          
 ## Questions
-        
-Feel free to reach my other project on https://github.com/2  
-Thank you for taking the time to review my project.  
-If you have any problem please do not hesitate to contact me with 2.
+    
+Thank you for taking the time to review my project. 
+Please feel free to reach my other project on https://github.com/1   
+If you have any problem please do not hesitate to contact me with 1.
--- Start/index.js
+++ Start/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 // const path = require('path');
 const inquirer = require("inquirer");
-// const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("../utils/generateMarkdown.js");
 
 // array of questions for user
 inquirer.prompt([
@@ -56,86 +56,26 @@ inquirer.prompt([
         name: 'license',
     },
   ])
-  .then((response) => {
-
-    switch (response.license) {
-        case 'Apache License' :
-        var licensePNG = "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
-        break;
-        case 'MIT License' :
-        var licensePNG = "https://img.shields.io/badge/License-MIT-yellow.svg";
-        break;
-        case 'BSD 2-Clause “Simplified” License' :
-        var licensePNG = "https://img.shields.io/badge/License-BSD%202--Clause-orange.svg";
-        break;
-        }
-
-    const gitlink = `https://github.com/${response.gitName}`;
-    const readme = `
-# ${response.title}
-
-## Table of Contents 
-        
-[Description](#Description)  &nbsp; | &nbsp;  [Installation](#Installation)  &nbsp; | &nbsp;  [Usage](#Usage)  &nbsp; | &nbsp;  [Contributing](#Contributing)  &nbsp; | &nbsp;  [Tests](#Tests)  
-        
-![alt](${licensePNG})
-
-## Description
-        
-${response.description}
-    
-## Installation
-    
-${response.installation}
-        
-## Usage
-    
-${response.usage}
-        
-## Contributing
-        
-${response.contributing}
-        
-## Tests
-        
-${response.tests}
-        
-## license
-
-The ${response.license} covers this application
-        
-## Questions
-        
-Feel free to reach my other project on ${gitlink}  
-Thank you for taking the time to review my project.  
-If you have any problem please do not hesitate to contact me with ${response.email}.`;
-
-fs.writeFile('./README.md',readme, (err) => 
-err ? console.error(err) : console.log('Commit logged!'))
-
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  .then((data) => {
 // // function to write README file
-// function writeToFile(fileName, data) {
-// }
+  fs.writeFile('./README.md', generateMarkdown(data), (err) =>
+  err ? console.error(err) : console.log('Success! ReadMe was generated!'));
+});
 
 // // function to initialize program
-// function init() {
+ function init() {
+  if(fs.existsSync('./README.md')){
+    inquirer.prompt([
+      {
+        type: 'list',
+        choices: ['Apache License', 'MIT License', 'BSD 2-Clause “Simplified” License'],
+        name: 'license',
+      },
 
-// }
+    fs.unlink('./image', (err) =>
+      err ? console.error(err) : console.log('deleted!')
+    )};
+ }
 
 // // function call to initialize program
-// init();
+ init();
